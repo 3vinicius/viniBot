baa675657666d7f74186143c87d460ea15bf11c6
Vinícius Amorim Alves Omena <63747127+3vinicius@users.noreply.github.com>
2024-07-24 13:30:05 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,22 +1,51 @@
 const {exec} = require('child_process')
+const {MessageMedia } = require('whatsapp-web.js');
+const fs = require("fs");
+const path = require("path");
 
+public async function dowloadMessageMedia(url, pitch) {
+    return await executeShell(url, pitch)
+        .then((resolve) => resolve)
+        .catch((err) => err)
+}
 
-function executeShell() {
-    return new Promise( async (resolve, reject) => {
-        const { stdout, stderr } = await exec('ls');
-        stdout.on("data", (data) => {
-            resolve(data.toString())
+private async function executeShell(url, pitch) {
+    try {
+        return new Promise( async (resolve, reject) => {
+            const { stdout, stderr } = await exec(`sh "./files/script.sh" ${url} ${pitch}`);
+            stdout.on("data", (data) => {
+                console.log(data)
+                resolve(createMessageMedia(data.toString()))
+            })
+            stderr.on('data', (data) => {
+                reject(data)
+            });
         })
-    })
+    } catch (err) {
+        return err
+    }
 }
 
 
-executeShell().then((resolve) => {
-        if(resolve.includes("test.mpeg")){
-            console.log("test.mpeg")
-        }
-    })
-    .catch((err) => {
-        console.log("Error:" + err);
-    })
 
+private function createMessageMedia(filename) {
+    try {
+        filePath = `./files/${filename}`
+        data = fs.readFileSync(filePath,'base64')
+        var stats = fs.statSync(filePath)
+        var fileSizeInBytes = stats.size;
+         return new MessageMedia('audio/mpeg',data, filename,fileSizeInBytes)
+    } catch (err) {
+        return err
+    }
+}
+
+
+// dowloadMessageMedia("test.mpeg", 9).then(re => {
+//             console.log(re)
+//
+//     }).catch(err => console.log(err))
+
+
+
+// console.log(createMessageMedia("test.mpeg"))
